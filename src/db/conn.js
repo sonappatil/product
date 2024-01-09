0c9a86c32a7bb269d356cf611dd19c3b39123490
Sonali Prakash Patil <106173021+sonappatil@users.noreply.github.com>
2024-01-09 16:10:13 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://******:*******@cluster0.w8m1b2p.mongodb.net/FrontEnd?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://******:*******@cluster0.w8m1b2p.mongodb.net/********?retryWrites=true&w=majority")
 .then(()=>{
     console.log("connect");
 }).catch((e)=>{
